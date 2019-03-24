1476613d5603c81844f508191cd6f4a02b765d38
Christophe Marchal <toff63@gmail.com>
2019-03-24 22:36:08 +0000

    Add readworld tutorial vue+typescript

---
+++ vue/real-world-app/realworld-vue-ts/src/router.ts
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/Login.vue'),
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('@/views/Register.vue'),
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: () => import('@/views/Settings.vue'),
+    },
+  ],
+});
